94e7e05ca2df3a492cc02c2b743c3472d700ee00
Thiago Zeni Bazotti <thiagozb@gmail.com>
2020-06-14 23:58:24 -0300

    Validating transaction type

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type must be income or outcome');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
